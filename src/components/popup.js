550ededd0f962536620cd51b73cb82d6ad1af07e
congtran8991 <45453055+congtran8991@users.noreply.github.com>
2020-04-14 12:34:19 +0700

    file component

---
+++ src/components/popup.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Popup from "reactjs-popup";
+ 
+export default () => (
+  <Popup  position="right center">
+      div
+    <div>Popup content here !!</div>
+  </Popup>
+);
---
+++ src/components/select.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function(){
+    $("#carerClass").clone().appendTo("#carerCopy");
+
+      $('select.selectpicker').selectpicker();
+      
+      
+  });
